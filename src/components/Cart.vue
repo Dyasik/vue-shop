824411cf16e2e0cde2389a77c17189c93e08e455
Denis Seletkov <denis.seletkov@piano.io>
2020-06-27 23:03:10 +0400

    Basic cart product

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -14,17 +14,22 @@
     <hr/>
 
     <div v-if="isCartEmpty" class="empty">
-      Корзина пуста. Добавить товары в корзину можно нажав на цену товара.
+      Корзина пуста. Добавить товары в корзину можно нажатием на цену товара.
     </div>
     <template v-else>
-      <div v-for="(products, category) in cartProducts" :key="category">
-        <div class="category">
+      <div v-for="(items, category) in cartProducts" :key="category">
+        <div class="cart-category">
           {{ category }}
         </div>
-        <div v-for="productEntry in products"
-            :key="productEntry.product.productId">
-          {{ productEntry.product.name }}: {{ productEntry.product.price }} * {{ productEntry.count }}
-        </div>
+
+        <CartProduct
+            v-for="cartItem in items"
+            :key="cartItem.product.productId"
+            :product-id="cartItem.product.productId"
+            :name="cartItem.product.name"
+            :price="cartItem.product.price"
+            :items-count="cartItem.count"
+        />
       </div>
 
       <div class="total">Итого: {{ total | formatPrice }}</div>
@@ -35,9 +40,11 @@
 <script>
   import { MUTATIONS } from '../store'
   import formatPrice from '../filters/formatPrice'
+  import CartProduct from './cart-components/CartProduct';
 
   export default {
     name: 'Cart',
+    components: {CartProduct},
     computed: {
       cartProducts: function () {
         return this.$store.getters.cartProducts
@@ -46,6 +53,7 @@
         return !Object.keys(this.$store.getters.cartProducts).length
       },
       total: function () {
+        // TODO: migrate to BigInt
         return Object.values(this.cartProducts)
           .flat()
           .reduce((sum, cartItem) => sum + cartItem.product.price * cartItem.count, 0)
@@ -84,5 +92,13 @@
 
   .empty {
     padding-top: 50px;
+  }
+
+  .cart-category {
+    background-color: #444;
+    width: fit-content;
+    padding: 4px 10px;
+    margin: 10px auto;
+    border-radius: 4px;
   }
 </style>
---
+++ src/components/cart-components/CartProduct.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="cart-product">
+    <div class="name">{{ name }}</div>
+    <div class="price">{{ price | formatPrice }}</div>
+    <div>&times;</div>
+    <div class="count">{{ itemsCount }}</div>
+    <div>&equals;</div>
+    <div class="cost">{{ cost | formatPrice }}</div>
+  </div>
+</template>
+
+<script>
+  import formatPrice from '../../filters/formatPrice'
+
+  export default {
+    name: 'CartProduct',
+    props: {
+      productId: {
+        type: Number,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      itemsCount: {
+        type: Number,
+        required: true,
+      },
+    },
+    computed: {
+      cost: function () {
+        return this.price * this.itemsCount
+      },
+    },
+    filters: {
+      formatPrice,
+    }
+  }
+</script>
+
+<style scoped>
+  .cart-product {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
